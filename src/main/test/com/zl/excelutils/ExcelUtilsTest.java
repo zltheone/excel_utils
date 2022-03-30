484dea884d4fac7d93f165c27b846c1df11484d3
li5.zhong <li5.zhong@tcl.com>
2022-03-30 12:18:13 +0800

    '新增map测试类'

--- src/main/test/com/zl/excelutils/ExcelUtilsTest.java
+++ src/main/test/com/zl/excelutils/ExcelUtilsTest.java
@@ -3,15 +3,17 @@ package com.zl.excelutils;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.zl.excelutils.domain.AdsOcLackMatFillInfoPO;
+import com.zl.excelutils.domain.ExportVO;
+import com.zl.excelutils.utils.DateUtils;
 import com.zl.excelutils.utils.ExcelUtils;
+import com.zl.excelutils.utils.ResponseUtils;
 import org.apache.commons.collections.CollectionUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @Description:
@@ -45,7 +47,30 @@ public class ExcelUtilsTest {
 
     @Test
     public void testExportMap(){
-
+        HashMap<String, Object> excelMap = new HashMap<>();
+        // 数据库操作
+        List<Map<String, Object>> list = new ArrayList<>(); // odfSuggestionMapper.selectPoForecastList(rbc, plant);
+        excelMap.put("datas", list);
+        HashMap<String, Object> headerMap = new HashMap<>();
+        headerMap.put("FCSTID", "FCSTID");
+        headerMap.put("BOM", "BOM");
+        headerMap.put("MODEL", "机型");
+        headerMap.put("ITEM_DESC", "BOM描述");
+        headerMap.put("FCSTDATE", "预测日期");
+        headerMap.put("FCSTWEEK", "预测周次");
+        headerMap.put("PLANT", "工厂");
+        headerMap.put("ORG_ID", "RBC");
+        headerMap.put("ORG_NAME", "RBC名称");
+        headerMap.put("CUSTOMER", "客户编号");
+        headerMap.put("QUANTITY", "原需求数量");
+        headerMap.put("ODF_QTY", "已开单数量");
+        headerMap.put("OPEN_QTY", "可开单数量");
+        headerMap.put("SYS_UPDATE_BY", "预测人");
+        excelMap.put("head", headerMap);
+        byte[] bytes = ExcelUtils.exportExcel(excelMap);
+        String date = DateUtils.formatDateToStr(new Date(), "yyyy-MM-dd");
+        ExportVO vo = new ExportVO("FCST预测_" + date, bytes);
+        // return ResponseUtils.setExcelResponse(response, vo); // 设置响应头
     }
 
 
